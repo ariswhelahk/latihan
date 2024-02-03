5bebab5efd2b3c004b51bf7db51b7dbd9307f1f5
aris <senior.aris1@gmail.com>
2024-02-03 10:47:31 +0700

    al hafidz jaya jaya jaya

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout')
+@section('content')
+    <div class="card"></div>
+        <div class="card-body">
+            <h3>Selamat Datang Di Halaman  Dashboard.</h3>
+        </div>
+    </div>
+@endsection
